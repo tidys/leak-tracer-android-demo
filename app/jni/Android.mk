0d520a98aca482ce211081f8b997ba1bea7b5693
xu_yanfeng <xu_yanfeng@126.com>
2023-11-06 13:58:56 +0800

    修复c++获取不到宏的bug

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,11 +4,9 @@ LOCAL_MODULE := jni
 LOCAL_SRC_FILES := ../src/main/cpp/native-lib.cpp
 LOCAL_LDFLAGS += -Wl,-Bsymbolic
 #--unresolved-symbols=ignore-all
-LOCAL_CFLAGS += -fPIC -g -ffunction-sections -fdata-sections
+LOCAL_CFLAGS += -fPIC -g -ffunction-sections -fdata-sections -DUSE_LEAK_TRACER=$(USE_LEAK_TRACER)
 LOCAL_CPPFLAGS += -g
 LOCAL_LDFLAGS += -Wl,--gc-sections
-LOCAL_EXPORT_CFLAGS := -D
-
 ifeq ($(USE_LEAK_TRACER), 1)
 LOCAL_SHARED_LIBRARIES += leaktracer
 endif
